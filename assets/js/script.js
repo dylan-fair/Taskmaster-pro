764d99d24a636f55fbe3209857b306eff99cb883
dylan fair <dylanfair11@gmail.com>
2021-09-22 16:04:22 -0500

    added urgancy colors to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+  auditTask(taskLi);
+  $("#list-" + taskList).append(taskLi);
 
 
   // append to ul list on the page
@@ -88,10 +90,16 @@ $(".list-group").on("click", "span", function(){
     .addClass("form-control")
     .val(date);
   $(this).replaceWith(dateInput);
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    } 
+  })
   dateInput.trigger("focus");
 })
 
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("change", "input[type='text']", function(){
   var date = $(this)
     .val()
     .trim();
@@ -108,6 +116,7 @@ $(".list-group").on("blur", "input[type='text']", function(){
     .addClass("badge badge-primary badge-pill")
     .text(date);
   $(this).replaceWith(taskSpan);
+  auditTask($(taskSpan).closest(".list-group-item"));
 })
 
 // modal was triggered
@@ -203,6 +212,24 @@ $("#trash").droppable({
     console.log("out")
   }
 });
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+var auditTask = function(taskEl){
+  var date = $(taskEl).find("span").text().trim();
+
+  var time = moment(date, "L").set("hour", 17);
+  
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if(moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days"))<= 2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
+
+}
 // load tasks for the first time
 loadTasks();
 
