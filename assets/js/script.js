43895d64c63c16c8b8d288927fc0a50367d657e4
dylan fair <dylanfair11@gmail.com>
2021-09-22 15:11:33 -0500

    added drag and drop and remove

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,6 +153,56 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+  },
+  deactivate: function(event){
+    console.log("deavtivate", this);
+  },
+  over: function(event){
+    console.log("over", event.target);
+  },
+  update: function(event){
+    var tempArr = [];
+    $(this).children().each(function(){
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    })
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+      ui.draggable.remove();
+  },
+  over : function(event, ui){
+    console.log("over")
+  },
+  out: function(event, ui) {
+    console.log("out")
+  }
+});
 // load tasks for the first time
 loadTasks();
 
